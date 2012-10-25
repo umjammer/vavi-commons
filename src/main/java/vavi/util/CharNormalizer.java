d7697fe7438059d9846bcde113cbb2f23e62bcd5
Naohide Sano <sano-n@klab.com>
2012-10-25 15:15:44 +0900

    forgot to commit

---
+++ src/main/java/vavi/util/CharNormalizer.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2012 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+package vavi.util;
+
+
+/**
+ * CharNormalizer. 
+ *
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
+ * @version 0.00 2012/10/21 umjammer initial version <br>
+ */
+public interface CharNormalizer {
+
+    /** */
+    String normalize(String string);
+}
+
+/* */
