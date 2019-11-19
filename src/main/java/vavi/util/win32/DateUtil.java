c0f271e88d23240e977c287415f44c163e5b1e26
umjammer <umjammer@gmail.com>
2019-11-19 19:36:20 +0900

    add new filetime converter

--- src/main/java/vavi/util/win32/DateUtil.java
+++ src/main/java/vavi/util/win32/DateUtil.java
@@ -6,12 +6,14 @@
 
 package vavi.util.win32;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 
 
 /**
  * Win32 の日付関連の構造体を扱うためのクラスです．
- * 何でこんなにいっぱいあるの? さすが M$、統一感無いねぇ。
  *
  * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (nsano)
  * @version 0.00 030211 nsano initial version <br>
@@ -69,13 +71,11 @@ public final class DateUtil {
     }
 
     /**
-     * DOS DATE (int) と DOS TIME (int) から
-     * java long (msec since 1970/1/1) を取得します．
+     * DOS DATE (int) と DOS TIME (int) から java long (msec since 1970/1/1)
+     * を取得します．
      *
-     * @param    date    上位から 7 bit が 1980 年からの年数，4 bit が月，
-     *            残りの 5 bit が日を表す
-     * @param    time    上位から 5 bit が時間，6 bit が分，
-     *            残りの 5 bit が秒を 2 で割ったもの
+     * @param date 上位から 7 bit が 1980 年からの年数，4 bit が月， 残りの 5 bit が日を表す
+     * @param time 上位から 5 bit が時間，6 bit が分， 残りの 5 bit が秒を 2 で割ったもの
      */
     public static final long dosDateTimeToLong(int date, int time) {
 
@@ -89,6 +89,29 @@ public final class DateUtil {
 //System.err.println(y + "/" + M + "/" + d + " " + h + ":" + m + ":" + s);
         calendar.set(y, M, d, h, m, s);
         return calendar.getTime().getTime();
+    }
+
+    /**
+     * @see "https://stackoverflow.com/questions/5398557/java-library-for-dealing-with-win32-filetime"
+     */
+    private static final Instant ZERO = Instant.parse("1601-01-01T00:00:00Z");
+
+    /**
+     * @see "https://stackoverflow.com/questions/5398557/java-library-for-dealing-with-win32-filetime"
+     * @return filetime
+     */
+    public static long toFileTime(long epochMillis) {
+        Duration duration = Duration.between(ZERO, Instant.ofEpochMilli(epochMillis));
+        return duration.getSeconds() * 10_000_000 + duration.getNano() / 100;
+    }
+
+    /**
+     * @see "https://stackoverflow.com/questions/5398557/java-library-for-dealing-with-win32-filetime"
+     * @return epoch millis
+     */
+    public static long fromFileTime(long fileTime) {
+        Duration duration = Duration.of(fileTime / 10, ChronoUnit.MICROS).plus(fileTime % 10 * 100, ChronoUnit.NANOS);
+        return ZERO.plus(duration).toEpochMilli();
     }
 }
 
