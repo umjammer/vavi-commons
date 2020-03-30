a828dd330a0154aebc572c4047eb66d003030cf5
umjammer <umjammer@gmail.com>
2020-03-30 14:25:49 +0900

    update drastically * search into super classes * search by name * refactoring

--- src/main/java/vavi/beans/BeanUtil.java
+++ src/main/java/vavi/beans/BeanUtil.java
@@ -12,6 +12,7 @@ import java.lang.reflect.Method;
 import java.security.AccessController;
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
+import java.util.logging.Logger;
 
 
 /**
@@ -22,7 +23,7 @@ import java.security.PrivilegedExceptionAction;
  */
 public abstract class BeanUtil {
 
-//  private static Log logger = LogFactory.getLog(CsvUtil.class);
+    private static Logger logger = Logger.getLogger(BeanUtil.class.getName());
 
     /**
      * 初めに通常に読めるフィールドで取得します。
@@ -35,38 +36,54 @@ public abstract class BeanUtil {
      * @param bean 取得対象のオブジェクト
      */
     public static Object getFieldValue(Field field, Object bean) {
-        Class<?> beanClass = bean.getClass();
-        Class<?> fieldClass = field.getType();
+        String name = field.getName();
+
         try {
             return field.get(bean);
         } catch (IllegalAccessException e) {
-            String fieldName = field.getName();
+logger.fine("no field: " + field.getName());
+        }
+
+        try {
+            return getByMethod(bean, name);
+        } catch (NoSuchMethodException e) {
+logger.fine("no method: " + name);
+        }
+
+        try {
+            return getByMethod(bean, getGetterName(name));
+        } catch (NoSuchMethodException e) {
+logger.fine("no method: " + getGetterName(name));
+        }
+
+        if (Boolean.TYPE.equals(field.getType())) {
             try {
-//logger.debug("method: " + "get" + Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1));
-                Method method = beanClass.getMethod("get" + Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1));
-                return method.invoke(bean);
-            } catch (NoSuchMethodException e2) {
-                if (Boolean.TYPE.equals(fieldClass)) {
-                    try {
-                        Method method = beanClass.getMethod("is" + Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1));
-                        return method.invoke(bean);
-                    } catch (InvocationTargetException e3) {
-                        throw new IllegalStateException(e3);
-                    } catch (NoSuchMethodException e3) {
-                        return getPrivateFieldValue(field, bean);
-//                      throw new IllegalStateException("no access method for: " + fieldName);
-                    } catch (IllegalAccessException e3) {
-                        throw new IllegalStateException(e3);
-                    }
-                } else {
-                    return getPrivateFieldValue(field, bean);
-//                  throw new IllegalStateException("no access method for: " + fieldName);
-                }
-            } catch (InvocationTargetException e2) {
-                throw new IllegalStateException(e2);
-            } catch (IllegalAccessException e2) {
-                throw new IllegalStateException(e2);
+                return getByMethod(bean, getBooleanGetterName(name));
+            } catch (NoSuchMethodException e) {
+logger.fine("no method: " + getBooleanGetterName(name));
             }
+        }
+
+        return getPrivateFieldValue(field, bean);
+    }
+
+    /** */
+    private static String getGetterName(String name) {
+        return "get" + Character.toUpperCase(name.charAt(0)) + name.substring(1);
+    }
+
+    /** */
+    private static String getBooleanGetterName(String name) {
+        return "is" + Character.toUpperCase(name.charAt(0)) + name.substring(1);
+    }
+
+    /** Recurse super classes. */
+    private static Object getByMethod(Object bean, String name) throws NoSuchMethodException {
+        try {
+            Method method = getMethodByNameOf(bean.getClass(), name);
+            return method.invoke(bean);
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            throw new IllegalStateException(e);
         }
     }
 
@@ -82,40 +99,88 @@ public abstract class BeanUtil {
      * @param value 設定する値
      */
     public static void setFieldValue(Field field, Object bean, Object value) {
-        Class<?> beanClass = bean.getClass();
-        Class<?> fieldClass = field.getType();
+        Class<?> valueClass = field.getType();
+        String name = field.getName();
+
         try {
             field.set(bean, value);
         } catch (IllegalAccessException e) {
-            String fieldName = field.getName();
-            try {
-//logger.debug("method: " + "set" + Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1));
-                Method method = beanClass.getMethod("set" + Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1), fieldClass);
-                method.invoke(bean, value);
-            } catch (NoSuchMethodException e2) {
-                setPrivateFieldValue(field, bean, value);
-//              throw new IllegalStateException("no access method for: " + fieldName);
-            } catch (InvocationTargetException e2) {
-                throw new IllegalStateException(e2);
-            } catch (IllegalAccessException e2) {
-                throw new IllegalStateException(e2);
-            }
+logger.fine("no field: " + name);
         }
+
+        try {
+            setByMethod(bean, name, valueClass, value);
+        } catch (NoSuchMethodException e) {
+logger.fine("no method: " + name);
+        }
+
+        try {
+            setByMethod(bean,  getSetterName(name), valueClass, value);
+        } catch (NoSuchMethodException e) {
+logger.fine("no method: " + getSetterName(name));
+        }
+
+        setPrivateFieldValue(field, bean, value);
     }
 
     /** */
-    private static Field getPrivateField(final Class<?> clazz, final String name) throws PrivilegedActionException {
+    private static String getSetterName(String name) {
+        return "set" + Character.toUpperCase(name.charAt(0)) + name.substring(1);
+    }
+
+    /** Recurse super classes. */
+    private static void setByMethod(Object bean, String name, Class<?> valueClass, Object value) throws NoSuchMethodException {
+        try {
+            Method method = getMethodByNameOf(bean.getClass(), name, valueClass);
+            method.invoke(bean, value);
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    /** Recurse super classes. */
+    public static Field getPrivateField(Class<?> clazz, String name) throws PrivilegedActionException {
         return AccessController.doPrivileged(new PrivilegedExceptionAction<Field>() {
             /** @throws NoSuchFieldException */
             public Field run() throws Exception {
-                Field field = clazz.getDeclaredField(name);
+                Field field = getFieldByNameOf(clazz, name);
                 field.setAccessible(true);
                 return field;
             }
         });
     }
 
-    /** */
+    /** Recurse super classes. */
+    public static Method getPrivateMethod(Class<?> clazz, String name, Class<?>[] argTypes) throws PrivilegedActionException {
+        return AccessController.doPrivileged(new PrivilegedExceptionAction<Method>() {
+            /** @throws NoSuchMethodException */
+            public Method run() throws Exception {
+                Method method = getMethodByNameOf(clazz, name, argTypes);
+                method.setAccessible(true);
+                return method;
+            }
+        });
+    }
+
+    /** Recurse super classes. */
+    public static Object invoke(Method method, Object bean, Object... args) {
+        try {
+            return method.invoke(bean, args);
+        } catch (IllegalAccessException e) {
+logger.fine("method access exception: " + method.getName());
+        } catch (InvocationTargetException e) {
+            throw new IllegalStateException(e);
+        }
+
+        try {
+            method = BeanUtil.getPrivateMethod(bean.getClass(), method.getName(), method.getParameterTypes());
+            return method.invoke(bean, args);
+        } catch (PrivilegedActionException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    /** Getter */
     private static Object getPrivateFieldValue(Field field, Object bean) {
         try {
             Field accessibleField = getPrivateField(field.getDeclaringClass(), field.getName());
@@ -127,7 +192,7 @@ public abstract class BeanUtil {
         }
     }
 
-    /** */
+    /** Setter */
     private static void setPrivateFieldValue(Field field, Object bean, Object value) {
         try {
             Field accessibleField = getPrivateField(field.getDeclaringClass(), field.getName());
@@ -135,6 +200,85 @@ public abstract class BeanUtil {
         } catch (IllegalAccessException e) {
             throw new IllegalStateException(e);
         } catch (PrivilegedActionException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    /** Recurse super classes. */
+    public static Field getFieldByNameOf(Class<?> clazz, String name) throws NoSuchFieldException {
+        Field field = null;
+        while (clazz != null) {
+            try {
+                field = clazz.getDeclaredField(name);
+                break;
+            } catch (NoSuchFieldException e) {
+                clazz = clazz.getSuperclass();
+            }
+        }
+        if (field == null) {
+            throw new NoSuchFieldException();
+        }
+        return field;
+    }
+
+    /** Recurse super classes. */
+    public static Method getMethodByNameOf(Class<?> clazz, String name, Class<?>... argTypes) throws NoSuchMethodException {
+        Method method = null;
+        while (clazz != null) {
+            try {
+                method = clazz.getDeclaredMethod(name, argTypes);
+                break;
+            } catch (NoSuchMethodException e) {
+                clazz = clazz.getSuperclass();
+            }
+        }
+        if (method == null) {
+            throw new NoSuchMethodException();
+        }
+        return method;
+    }
+
+    /**
+     * 初めに通常に読めるフィールドで取得します。
+     * 次に Getter メソッド(Bean命名規則, booleanの場合isFooあり)で取得します。
+     * 最後に private フィールドを強制的に取得します。
+     *
+     * TODO use {@link java.beans.Introspector} ??? or "org.apache.commons.beanutils.BeanUtils"
+     *
+     * @param name 対象となるフィールドもしくはメソッドの名前
+     * @param bean 取得対象のオブジェクト
+     */
+    public static Object getValue(String name, Object bean) {
+        try {
+            Field field = getFieldByNameOf(bean.getClass(), name);
+            return getFieldValue(field, bean);
+        } catch (NoSuchFieldException e) {
+logger.fine("no field: " + name);
+        }
+
+        try {
+            return getByMethod(bean, name);
+        } catch (NoSuchMethodException e) {
+logger.fine("no method: " + name);
+        }
+
+        try {
+            return getByMethod(bean, getGetterName(name));
+        } catch (NoSuchMethodException e) {
+logger.fine("no method: " + getGetterName(name));
+        }
+
+        try {
+            return getByMethod(bean, getBooleanGetterName(name));
+        } catch (NoSuchMethodException e) {
+logger.fine("no method: " + getBooleanGetterName(name));
+        }
+
+        try {
+            Field field = getFieldByNameOf(bean.getClass(), name);
+            return getPrivateFieldValue(field, bean);
+        } catch (NoSuchFieldException e) {
+logger.fine("no private field: " + name);
             throw new IllegalStateException(e);
         }
     }
