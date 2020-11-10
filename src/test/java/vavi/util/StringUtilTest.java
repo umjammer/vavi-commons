9ad5c4c0643c42d43ae1638642a0e385d6a94ef6
umjammer <umjammer@gmail.com>
2020-11-10 12:24:36 +0900

    update about dump

--- src/test/java/vavi/util/StringUtilTest.java
+++ src/test/java/vavi/util/StringUtilTest.java
@@ -29,6 +29,27 @@ class StringUtilTest {
         assertEquals("**...**.", StringUtil.toBits(0xc6, 8));
     }
 
+    @Test
+    void test2() {
+        byte[] dump = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+        System.err.println(StringUtil.getDump(dump));
+        System.err.println(StringUtil.getDump(dump, 16));
+        System.err.println(StringUtil.getDump(dump, 4));
+        System.err.println(StringUtil.getDump(dump, 4, 4));
+        System.err.println(StringUtil.getDump(dump, 0));
+        String expexted1 = "00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F    ................\n" +
+                           "10 11                                              ..";
+        assertEquals(expexted1, StringUtil.getDump(dump));
+        String expexted2 = "00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F    ................";
+        assertEquals(expexted2, StringUtil.getDump(dump, 16));
+        String expexted3 = "00 01 02 03                                        ....";
+        assertEquals(expexted3, StringUtil.getDump(dump, 4));
+        String expexted4 = "04 05 06 07                                        ....";
+        assertEquals(expexted4, StringUtil.getDump(dump, 4, 4));
+        String expexted5 = "";
+        assertEquals(expexted5, StringUtil.getDump(dump, 0));
+    }
+
     //----
 
     /** */
